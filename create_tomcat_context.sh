c9c169f65002f96084b440d4765f213bf98d57f9
Sharma <E0773739@eaton.ad.etn.com>
2024-03-06 21:17:28 +0530

    Initial Commit

--- create_tomcat_context.sh
+++ create_tomcat_context.sh
@@ -10,13 +10,15 @@ HOST=${MYSQL_HOST}
 PORT=${MYSQL_PORT}
 DATABASE=${MYSQL_DATABASE}
 USER=${MYSQL_USER}
-PASSWORD=${MYSQL_PASSWORD}
+PASSWORD=${DB_PASSWORD}
 
 echo "=> add a JNDI datasource in Tomcat"
+echo "1=> ${DB_PASSWORD}"
+echo "2=> ${PASSWORD}"
 sed -i -r 's/<\/Context>//' ${CATALINA_HOME}/conf/context.xml
 echo "<Resource name=\"jdbc/tapDB\" auth=\"Container\" type=\"javax.sql.DataSource\"" >> ${CATALINA_HOME}/conf/context.xml
 echo 'maxTotal="100" maxIdle="30" maxWaitMillis="10000"' >> ${CATALINA_HOME}/conf/context.xml
-echo "username=\"OEMAPNEWUSER\" password=\"sdfjhl758\" driverClassName=\"oracle.jdbc.OracleDriver\"" >> ${CATALINA_HOME}/conf/context.xml
+echo "username=\"OEMAPNEWUSER\" password=\"${PASSWORD}\" driverClassName=\"oracle.jdbc.OracleDriver\"" >> ${CATALINA_HOME}/conf/context.xml
 echo "url=\"jdbc:oracle:thin:@egsmalld-lsnr-db.tcc.etn.com:1521:WTCRD810\"/>" >> ${CATALINA_HOME}/conf/context.xml
 echo '</Context>' >> ${CATALINA_HOME}/conf/context.xml
 echo "=> Done!"
