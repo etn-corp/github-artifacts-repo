1f7af670472cae8eaa5b93760a2580c879ef1d51
Sharma <E0773739@eaton.ad.etn.com>
2024-03-07 15:13:50 +0530

    Initial Commit

--- create_tomcat_context.sh
+++ create_tomcat_context.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-if [ -f /tomcat_context_created ]; then
-    echo "JNDI datasource already added in Tomcat context.xml"
-    exit 0
-fi
-
 echo "=> add a JNDI datasource in Tomcat"
-echo "1=> ${DB_URL}"
-echo "2=> ${DB_PASSWORD}"
 sed -i -r 's/<\/Context>//' ${CATALINA_HOME}/conf/context.xml
 echo "<Resource name=\"jdbc/tapDB\" auth=\"Container\" type=\"javax.sql.DataSource\"" >> ${CATALINA_HOME}/conf/context.xml
 echo 'maxTotal="100" maxIdle="30" maxWaitMillis="10000"' >> ${CATALINA_HOME}/conf/context.xml
@@ -15,4 +8,3 @@ echo "username=\"OEMAPNEWUSER\" password=\"${DB_PASSWORD}\" driverClassName=\"or
 echo "url=\"${DB_URL}\"/>" >> ${CATALINA_HOME}/conf/context.xml
 echo '</Context>' >> ${CATALINA_HOME}/conf/context.xml
 echo "=> Done!"
-touch /tomcat_context_created
