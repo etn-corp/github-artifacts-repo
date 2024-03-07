21c69648cb011a9c46d928b6e5d935ee524c1735
Sharma <E0773739@eaton.ad.etn.com>
2024-03-07 14:48:19 +0530

    Initial Commit

--- create_tomcat_context.sh
+++ create_tomcat_context.sh
@@ -12,7 +12,7 @@ sed -i -r 's/<\/Context>//' ${CATALINA_HOME}/conf/context.xml
 echo "<Resource name=\"jdbc/tapDB\" auth=\"Container\" type=\"javax.sql.DataSource\"" >> ${CATALINA_HOME}/conf/context.xml
 echo 'maxTotal="100" maxIdle="30" maxWaitMillis="10000"' >> ${CATALINA_HOME}/conf/context.xml
 echo "username=\"OEMAPNEWUSER\" password=\"${DB_PASSWORD}\" driverClassName=\"oracle.jdbc.OracleDriver\"" >> ${CATALINA_HOME}/conf/context.xml
-echo "url=\"${DB_URL}"/>" >> ${CATALINA_HOME}/conf/context.xml
+echo "url=\"${DB_URL}\"/>" >> ${CATALINA_HOME}/conf/context.xml
 echo '</Context>' >> ${CATALINA_HOME}/conf/context.xml
 echo "=> Done!"
 touch /tomcat_context_created
